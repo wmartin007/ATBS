f7df8541308b1275311224206973069e4d10390e
Unknown <wmartin007@gmail.com>
2018-01-14 16:51:14 -0500

    1/14/2018 ATBS exercises - chp 3 no 2

--- collatz_ch3pp.py
+++ collatz_ch3pp.py
@@ -1,1 +1,25 @@
-# ATBS-Pra
+# ATBS-Practice Projects-Chapter 3: Collatz Sequence
+
+""" Write a function that accepts one parameter, number. If the number is
+even, the function should print the number//2 and if it's odd it should print
+3*number + 1"""
+
+
+def collatz(number):
+
+    if number % 2 == 0:
+        result = number // 2
+        print(result)
+        return result
+    elif number % 2 == 1:
+        result = 3 * number + 1
+        print(result)
+        return result
+
+def main():
+    guess = int(input("Enter an integer:"))
+
+    if guess != 1:
+        collatz(guess)
+
+main()
