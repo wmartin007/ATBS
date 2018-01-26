9fb00b517a4225dedb6d22333b8c5ed0f0c039d4
Unknown <wmartin007@gmail.com>
2018-01-26 15:01:20 -0500

    1/26/2018 ATBS chp4 - character pic grid update

---
+++ characterPictureGrid.py
@@ -0,0 +1,20 @@
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+
+rows = len(grid) # Number of elements in the list
+cols = len(grid[0]) # Number of elements in every single element in the list
+
+for j in range(cols):
+    for i in range(rows):
+        print(grid[i][j], end='')
+    print()
+
+
